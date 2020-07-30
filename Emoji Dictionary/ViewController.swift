fdc213689e4321c995f0d30696f6ca1d76483454
Ben Leimbach <benleimbach@gmail.com>
2020-07-29 21:14:38 -0600

    added a table view and working on the second screen

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var primarytableview: UITableView!
+    
+    var emojis = ["😂","🍤","🏕","🌈","💧","😅","🙃","☝️"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        primarytableview.dataSource = self
+        primarytableview.delegate = self
+        
+        
+    
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+       
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
 
 }
+
 
