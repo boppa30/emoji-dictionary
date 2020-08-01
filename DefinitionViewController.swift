bc67aecec477f9f26ded877158b5fd357d022c75
Ben Leimbach <benleimbach@gmail.com>
2020-07-31 22:28:01 -0600

    Created 2nd viewcontroller

---
+++ DefinitionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Ben Leimbach on 7/31/20.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
